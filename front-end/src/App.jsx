617466ff90ec9c16039ed5e805760bb2b7df2844
talal4a <talalxus000@gmail.com>
2025-07-22 12:05:08 +0500

    update

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,7 +1,6 @@
 import { Routes, Route, useLocation } from 'react-router-dom';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { SocketProvider } from './context/SocketProvider';
 import PrivateRoute from './components/PrivateRoute';
 import GuestRoute from './components/GuestRoute';
 import MainLayout from './MainLayout';
@@ -17,135 +16,117 @@ import ForgotPassword from './pages/ForgotPassword';
 import ResetPassword from './pages/ResetPassword';
 import NotFound from './components/NotFound';
 import { fetchWatchlist } from './slice/watchListSlice';
-import Watchroom from './pages/Watchroom';
-
 function App() {
   const { pathname } = useLocation();
   const dispatch = useDispatch();
   const user = useSelector((state) => state.user?.user);
-
-  // Reset scroll to top on route change
   useEffect(() => {
     requestAnimationFrame(() => {
       window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
     });
   }, [pathname]);
-
-  // Fetch watchlist when user is authenticated
   useEffect(() => {
     if (user) {
       dispatch(fetchWatchlist());
     }
   }, [user, dispatch]);
-
   return (
-    <SocketProvider>
-      <main className="min-h-screen bg-black text-white">
-        <Routes>
-          <Route element={<MainLayout />}>
-            <Route
-              path="/"
-              element={
-                <PrivateRoute>
-                  <Home />
-                </PrivateRoute>
-              }
-            />
-            <Route
-              path="/movie/:id"
-              element={
-                <PrivateRoute>
-                  <MovieDetail />
-                </PrivateRoute>
-              }
-            />
-            <Route
-              path="/my-list"
-              element={
-                <PrivateRoute>
-                  <WatchListPage />
-                </PrivateRoute>
-              }
-            />
-            <Route
-              path="/account"
-              element={
-                <PrivateRoute>
-                  <Account />
-                </PrivateRoute>
-              }
-            />
-            <Route
-              path="/admin"
-              element={
-                <PrivateRoute>
-                  <AdminDashboard />
-                </PrivateRoute>
-              }
-            />
-            <Route
-              path="/watch/:roomId"
-              element={
-                <PrivateRoute>
-                  <Watchroom />
-                </PrivateRoute>
-              }
-            />
-            <Route
-              path="/search"
-              element={
-                <PrivateRoute>
-                  <SearchPage />
-                </PrivateRoute>
-              }
-            />
-            <Route
-              path="*"
-              element={
-                <PrivateRoute>
-                  <NotFound />
-                </PrivateRoute>
-              }
-            />
-          </Route>
-          
-          {/* Public routes */}
+    <main className="min-h-screen bg-black text-white">
+      <Routes>
+        <Route element={<MainLayout />}>
           <Route
-            path="/auth/login"
+            path="/"
             element={
-              <GuestRoute>
-                <Login />
-              </GuestRoute>
+              <PrivateRoute>
+                <Home />
+              </PrivateRoute>
             }
           />
           <Route
-            path="/auth/signup"
+            path="/movie/:id"
             element={
-              <GuestRoute>
-                <Signup />
-              </GuestRoute>
+              <PrivateRoute>
+                <MovieDetail />
+              </PrivateRoute>
             }
           />
           <Route
-            path="/forgot-password"
+            path="/my-list"
             element={
-              <GuestRoute>
-                <ForgotPassword />
-              </GuestRoute>
+              <PrivateRoute>
+                <WatchListPage />
+              </PrivateRoute>
             }
           />
           <Route
-            path="/reset-password/:token"
+            path="/account"
             element={
-              <GuestRoute>
-                <ResetPassword />
-              </GuestRoute>
+              <PrivateRoute>
+                <Account />
+              </PrivateRoute>
             }
           />
-        </Routes>
-      </main>
-    </SocketProvider>
+          <Route
+            path="/admin"
+            element={
+              <PrivateRoute>
+                <AdminDashboard />
+              </PrivateRoute>
+            }
+          />
+
+          <Route
+            path="/search"
+            element={
+              <PrivateRoute>
+                <SearchPage />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="*"
+            element={
+              <PrivateRoute>
+                <NotFound />
+              </PrivateRoute>
+            }
+          />
+        </Route>
+
+        <Route
+          path="/auth/login"
+          element={
+            <GuestRoute>
+              <Login />
+            </GuestRoute>
+          }
+        />
+        <Route
+          path="/auth/signup"
+          element={
+            <GuestRoute>
+              <Signup />
+            </GuestRoute>
+          }
+        />
+        <Route
+          path="/forgot-password"
+          element={
+            <GuestRoute>
+              <ForgotPassword />
+            </GuestRoute>
+          }
+        />
+        <Route
+          path="/reset-password/:token"
+          element={
+            <GuestRoute>
+              <ResetPassword />
+            </GuestRoute>
+          }
+        />
+      </Routes>
+    </main>
   );
 }
-
 export default App;
--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -8,18 +8,15 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
 import { ToastProvider } from './context/ToastContext';
-import { SocketProvider } from './context/SocketProvider';
 const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
         <Provider store={store}>
-          <SocketProvider>
-            <ToastProvider>
-              <App />
-            </ToastProvider>
-          </SocketProvider>
+          <ToastProvider>
+            <App />
+          </ToastProvider>
         </Provider>
         <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
