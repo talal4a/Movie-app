ccde3192738f40660606611650abf1f43980d51c
talal4a <talalxus000@gmail.com>
2025-07-25 18:49:58 +0500

    update

--- front-end/src/pages/MoviePage.jsx
+++ front-end/src/pages/MoviePage.jsx
@@ -1,8 +1,8 @@
 import GroupedMovieCollections from '@/components/CollectionMovie';
 export default function MoviesPage() {
   return (
-    <main className="px-10  py-10">
-      <section className='my-10'>
+    <main className="p-10  ">
+      <section className="my-10">
         <GroupedMovieCollections />
       </section>
     </main>
