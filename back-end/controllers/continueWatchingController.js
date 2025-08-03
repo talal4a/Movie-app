84a44eab367c3bb3e5024a23614a43a4318e06de
talal4a <talalxus000@gmail.com>
2025-08-03 16:17:03 +0500

    update

---
+++ back-end/controllers/continueWatchingController.js
@@ -0,0 +1,20 @@
+const ContinueWatching = require("../models/ContinueWatching");
+exports.markAsWatched = async (req, res) => {
+  const { movieId } = req.body;
+  if (!movieId) {
+    return res.status(400).json({ message: "Movie ID is required." });
+  }
+  await ContinueWatching.findOneAndUpdate(
+    { user: req.user.id, movie: movieId },
+    { lastWatchedAt: new Date() },
+    { upsert: true, new: true, setDefaultsOnInsert: true }
+  );
+
+  res.status(200).json({ message: "Marked as watched" });
+};
+exports.getContinueWatching = async (req, res) => {
+  const list = await ContinueWatching.find({ user: req.user.id })
+    .populate("movie")
+    .sort({ lastWatchedAt: -1 });
+  res.status(200).json({ list });
+};
