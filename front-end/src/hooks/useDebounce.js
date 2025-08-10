2d74b8b5bce918407e2e4ee6e0220307338005df
talal4a <talalxus000@gmail.com>
2025-08-10 13:25:09 +0500

    update

--- front-end/src/hooks/useDebounce.js
+++ front-end/src/hooks/useDebounce.js
@@ -1,8 +1,6 @@
 import { useEffect, useState } from 'react';
-
 export function useDebounce(value, delay) {
   const [debouncedValue, setDebouncedValue] = useState(value);
-
   useEffect(() => {
     const handler = setTimeout(() => {
       setDebouncedValue(value);
